7b2a1e88e668366505a61de3c34a6f1359478223
Ilya Selyutin <leavetobefree777@gmail.com>
2025-09-30 00:52:55 +1000

    Обновил маршруты

--- config/routes.php
+++ config/routes.php
@@ -1,12 +1,12 @@
 <?php
 
+use App\Router\Route;
+
 return [
-    '/home' => function() {
-        include_once APP_PATH . '/../views/pages/home.php';
-    },
-        '/home' => function() {
-        include_once APP_PATH . '/../views/pages/movies.php';
-    },
+    Route::get('/home', function () {
+        include_once APP_PATH . '/views/pages/home.php';
+    }),
+    Route::get('/movies', function () {
+        include_once APP_PATH . '/views/pages/movies.php';
+    }),
 ];
-
-
