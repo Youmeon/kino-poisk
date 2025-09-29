8daa3752a4fa126078829ce382dc3a75ed2bb8dc
Ilya Selyutin <leavetobefree777@gmail.com>
2025-09-30 00:41:55 +1000

    Добавлен класс Route со статическими методами

---
+++ src/Route.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Router;
+
+// создаём класс Route, каждый объект этого класса будет хранить информацию об одном URL
+class Route
+{
+    public function __construct(
+        private string $uri,        //private объявляет свойства класса и присваивает им значение (элементы маршрута)
+        private string $method,
+        private $action
+    ) {}
+
+    // метод вызывается на классе, а не на объекте. Принимает $url и $action(обработчик маршрута) 
+    // возвращает new static (создаёт новый объект текущего класса)
+    public static function get(string $uri, $action): static
+    {
+        return new static($uri, 'GET', $action);
+    }
+
+    public static function post(string $uri, $action): static
+    {
+        return new static($uri, 'POST', $action);
+    }
+}
