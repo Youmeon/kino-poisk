ee82df47e5e3b479bed3a5b20fb5d03e012a77ad
Ilya Selyutin <leavetobefree777@gmail.com>
2025-09-30 17:52:13 +1000

    Поработал с инкапсуляцией, добавил комментарии. Спрятал глобальные переменные (Request.php)

--- src/App.php
+++ src/App.php
@@ -14,7 +14,7 @@ class App
         // dd($request); содержит все данные суперглобальных переменных
         // $uri = $_SERVER['REQUEST_URI'];
         // $method = $_SERVER['REQUEST_METHOD'];
-        // dd($request->uri());
+        // dd($request->uri()); нужно убрать параметр
         $router->dispatch($request->uri(), $request->method());
     }
 }
