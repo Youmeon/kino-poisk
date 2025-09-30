ee89cb43e02a68195e90ba3de5b367817243d80a
Ilya Selyutin <leavetobefree777@gmail.com>
2025-09-30 17:50:20 +1000

    Поработал с инкапсуляцией, добавил комментарии. Спрятал глобальные переменные (Request.php)

--- src/App.php
+++ src/App.php
@@ -3,14 +3,18 @@
 namespace App;
 
 use App\Router;
+use App\http\Request;
 
 class App
 {
     public function run(): void
     {
         $router = new Router();
-        $uri = $_SERVER['REQUEST_URI'];
-        $method = $_SERVER['REQUEST_METHOD'];
-        $router->dispatch($uri, $method);
+        $request = Request::createFromGlobals();
+        // dd($request); содержит все данные суперглобальных переменных
+        // $uri = $_SERVER['REQUEST_URI'];
+        // $method = $_SERVER['REQUEST_METHOD'];
+        // dd($request->uri());
+        $router->dispatch($request->uri(), $request->method());
     }
 }
---
+++ src/http/Request.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\http;
+
+class Request
+{
+    public function __construct(
+        public readonly array $get, #публичные, можем читать но изменять не можем
+        public readonly array $post,
+        public readonly array $server,
+        public readonly array $files,
+        public readonly array $cookies,
+    ) {}
+    public static function createFromGlobals(): self
+    {
+        return new self(
+            $_GET,
+            $_POST,
+            $_SERVER,
+            $_FILES,
+            $_COOKIE,
+        );
+    }
+
+    public function uri(): string
+    {
+        //убираем лишнюю информацию (указываем некоторую строку и элемент после которого строка будет обрезана)
+        return strtok($this->server['REQUEST_URI'], `?`);
+    }
+    public function method(): string
+    {
+        return $this->server['REQUEST_METHOD'];
+    }
+}
