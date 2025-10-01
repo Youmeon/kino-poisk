2fca923890f570c2c0a5c4c66beca260958ccc4c
Ilya Selyutin <leavetobefree777@gmail.com>
2025-10-01 20:29:16 +1000

    внедрил валидатор в request

--- Kernel/http/Request.php
+++ Kernel/http/Request.php
@@ -2,8 +2,12 @@
 
 namespace App\Kernel\http;
 
+use App\Kernel\Validator\Validator;
+
 class Request
 {
+    private Validator $validator;
+
     public function __construct(
         public readonly array $get, #публичные, можем читать но изменять не можем
         public readonly array $post,
@@ -35,5 +39,24 @@ class Request
     public function input(string $key, $default = null): mixed //дефолтное значение в случае если не получим какое то значение 
     {
         return $this->post[$key] ?? $this->get[$key] ?? $default; //если в массиве ничего не найдено возвращаем $default, перед этим получаем значене из get
+    }
+
+    public function setValidator(Validator $validator): void
+    {
+        $this->validator = $validator;
+    }
+    public function validate(array $rules): bool
+    {
+        $data = [];
+
+        foreach ($rules as $field => $rule) {
+            $data[$field] = $this->input($field);
+        }
+
+        return $this->validator->validate($data, $rules);
+    }
+    public function errors(): array
+    {
+        return $this->validator->errors();
     }
 }
